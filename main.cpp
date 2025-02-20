95fdd01acbdd27a8d970ce4e4b0fd886da71203e
Hayk10002 <hayk.g.khachatryan@gmail.com>
2025-02-20 13:53:13 +0400

    Add output formatting

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <list>
 #include <chrono>
 #include <random>
+#include <format>
 
 // Random number from 0 to max-1 inclusive
 int getRandomNum(int max)
@@ -36,22 +37,33 @@ int time_ms(F f, int N)
 {
     auto start = std::chrono::system_clock::now();
     for (int i = 0; i < N; i++) f();
-    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count();
+    return (int)std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count();
 }
 
 int main() {
     int N;
     std::cout << "Enter number count: ";
     std::cin >> N;
+    std::cout << std::endl;
 
     std::vector<int> v;
     std::list<int> l;
 
-    std::cout << "Vec Add: " << time_ms([&v](){ AddNum(v, getRandomNum(1000)); }, N) << "ms\n";
-    std::cout << "Vec Rem: " << time_ms([&v](){ RemoveInd(v,  getRandomNum(v.size())); }, N) << "ms\n";
-    std::cout << "List Add: " << time_ms([&l](){ AddNum(l, getRandomNum(1000)); }, N) << "ms\n";
-    std::cout << "List Rem: " << time_ms([&l](){ RemoveInd(l, getRandomNum(l.size())); }, N) << "ms\n";
+    int v_add_time = time_ms([&v](){ AddNum   (v, getRandomNum((int)1000    )); }, N);
+    int v_rem_time = time_ms([&v](){ RemoveInd(v, getRandomNum((int)v.size())); }, N);
+    int l_add_time = time_ms([&l](){ AddNum   (l, getRandomNum((int)1000    )); }, N);
+    int l_rem_time = time_ms([&l](){ RemoveInd(l, getRandomNum((int)l.size())); }, N);
+    int v_time = v_add_time + v_rem_time;
+    int l_time = l_add_time + l_rem_time;
+    
+    std::cout << std::format(
+        "         |   Add   |  Remove  |   Sum   \n"
+        "---------|---------|----------|---------\n"
+        "Vector   | {0:6}ms|  {1:6}ms| {4:6}ms\n"
+        "---------|---------|----------|---------\n"
+        "List     | {2:6}ms|  {3:6}ms| {5:6}ms\n",
+        v_add_time, v_rem_time, l_add_time, l_rem_time, v_time, l_time
+    );
 
-    system("pause");
     return 0;
 }
